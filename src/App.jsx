f458172c81dc8f372f68fdf0e52a82044597693d
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2023-11-27 12:05:34 -0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
 import ContactList from "./components/ContactList"
+import SelectedContact from "./components/SelectedContact"
+ 
+export default function App() {
+  const [setSelectedContactId, setSelectedContactId] = useState(null);
+const [firstname, setFirstName] = useState(``);
 
-
-function App() {
     return (
       <>
-        <ContactList />
-      </>
+      <form>
+        <label>Name</label><input name = "firstname" onChange={(event) => setFirstName(event.target.value)}></input>
+      </form>
+
+      {setSelectedContactId ? (
+        <SelectedContact
+        selectedContactId={selectedContactId}
+        setSelectedContactId={setSelectedContactId}
+        />
+      ):(
+        <ContactList selectedContactId ={setSelectedContactId}/>
+      )}
+        </>
     );
   }
-  export default App;
